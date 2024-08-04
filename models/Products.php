9e033a832b81db650464811f44919960bc8f1a6c
beemo02 <reyangelobanares.carullo@gmail.com>
2024-08-04 13:20:49 +0800

    initial commit with product crud

---
+++ models/Products.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "products".
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $name
+ * @property string|null $description
+ * @property float $price
+ * @property int $stock
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Categories $category
+ * @property OrderItems[] $orderItems
+ */
+class Products extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'products';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'name', 'price', 'stock'], 'required'],
+            [['category_id', 'stock'], 'integer'],
+            [['description'], 'string'],
+            [['price'], 'number'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Categories::class, 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'price' => 'Price',
+            'stock' => 'Stock',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Categories::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[OrderItems]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrderItems()
+    {
+        return $this->hasMany(OrderItems::class, ['product_id' => 'id']);
+    }
+}
